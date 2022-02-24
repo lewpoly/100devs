8636586be3f2df0e316608b4a7c9110416f51c88
lewpoly <ldixon.design@gmail.com>
2022-02-23 21:17:56 -0500

    after week 13

---
+++ materials/class13/temperature-converter/js/main.js
@@ -0,0 +1,11 @@
+//Write your pseduo code first! 
+
+
+//convert temperature from celcius to farenheit
+
+// establish what the temperature is in celcius through user input
+document.querySelector()
+
+// create a function that mathematically converts the temperature
+
+// display the temperature
