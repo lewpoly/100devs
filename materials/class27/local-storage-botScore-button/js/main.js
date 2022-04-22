f9c7a6c8d58e941b1e38261353552cce098dd741
Lew Dixon <ldixon.design@gmail.com>
2022-04-22 16:01:51 -0400

    testing eslint

--- materials/class27/local-storage-botScore-button/js/main.js
+++ materials/class27/local-storage-botScore-button/js/main.js
@@ -1,6 +1,6 @@
 //Create a button that adds 1 to a botScore stored in localStorage
-if(!localStorage.getItem('botScore')){
-  localStorage.setItem('botScore', 0)
+if (!localStorage.getItem('botScore')) {
+  localStorage.setItem('botScore', 0);
 }
 
 document.querySelector('button').addEventListener('click', addAnothaOne);
