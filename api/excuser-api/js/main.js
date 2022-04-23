e69849316386152742021d456c5a554f1fc78399
lewpoly <ldixon.design@gmail.com>
2022-04-22 22:59:07 -0400

    excuses-api

--- api/excuser-api/js/main.js
+++ api/excuser-api/js/main.js
@@ -1,15 +1,46 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch);
+document.getElementById('family').addEventListener('click', getFetch);
+document.getElementById('office').addEventListener('click', getFetch);
+document.getElementById('children').addEventListener('click', getFetch);
+document.getElementById('college').addEventListener('click', getFetch);
+document.getElementById('party').addEventListener('click', getFetch);
 
 function getFetch() {
-  const choice = document.querySelector('input').value;
-  console.log(choice);
-  const url = `=${choice}`;
+  const urlFamily = `https://excuser.herokuapp.com/v1/excuse/family`;
+  const urlOffice = `https://excuser.herokuapp.com/v1/excuse/office`;
+  const urlChildren = `https://excuser.herokuapp.com/v1/excuse/children`;
+  const urlCollege = `https://excuser.herokuapp.com/v1/excuse/college`;
+  const urlParty = `https://excuser.herokuapp.com/v1/excuse/party`;
 
-  fetch(url)
+  fetch(urlFamily)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      document.querySelector('h2').innerHTML = data[0].excuse;
+    });
+  fetch(urlOffice)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      document.querySelector('h2').innerHTML = data[0].excuse;
+    });
+  fetch(urlChildren)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      document.querySelector('h2').innerHTML = data[0].excuse;
+    });
+  fetch(urlCollege)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      document.querySelector('h2').innerHTML = data[0].excuse;
+    })
+  fetch(urlParty)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      document.querySelector('h2').innerHTML = data[0].excuse;
     })
     .catch((err) => {
       console.log(`error ${err}`);
