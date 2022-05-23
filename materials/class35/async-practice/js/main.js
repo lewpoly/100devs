678abed2a1a62e6051344d171cb4ea4a45b84847
lewpoly <ldixon.design@gmail.com>
2022-05-22 21:28:10 -0400

    small changes

--- materials/class35/async-practice/js/main.js
+++ materials/class35/async-practice/js/main.js
@@ -100,37 +100,37 @@
 
 //Code 07
 // function houseOne() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve('Paper delivered to house 1');
-//     }, 1000);
-//   });
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 1');
+//         }, 1000);
+//     });
 // }
 // function houseTwo() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve('Paper delivered to house 2');
-//     }, 5000);
-//   });
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 2');
+//         }, 5000);
+//     });
 // }
 // function houseThree() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve('Paper delivered to house 3');
-//     }, 2000);
-//   });
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 3');
+//         }, 2000);
+//     });
 // }
 //
 // async function getPaid() {
-//   const houseOneWait = await houseOne();
-//   const houseTwoWait = await houseTwo();
-//   const houseThreeWait = await houseThree();
-//   console.log(houseOneWait);
-//   console.log(houseTwoWait);
-//   console.log(houseThreeWait);
+//     const houseOneWait = await houseOne();
+//     const houseTwoWait = await houseTwo();
+//     const houseThreeWait = await houseThree();
+//     console.log(houseOneWait);
+//     console.log(houseTwoWait);
+//     console.log(houseThreeWait);
 // }
 
-// getPaid();
+getPaid();
 
 // TODO: add a try...catch to this function
 //Code 08
