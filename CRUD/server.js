e340a414922c9730d82b802d9b3bc71aa845d6d9
lewpoly <ldixon.design@gmail.com>
2022-05-25 19:39:38 -0400

    added crud and gitignore

---
+++ CRUD/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+console.log('May Node be with you.');
+app.listen(3000, function () {
+  console.log('listening on 3000');
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
