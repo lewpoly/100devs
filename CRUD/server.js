24095061f401d5d8e6db84cbcfa86b337af553de
lewpoly <ldixon.design@gmail.com>
2022-05-28 17:54:41 -0400

    class 37

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('listening on 3000');
 });
 app.get('/', (req, res) => {
+  // eslint-disable-next-line no-undef
   res.sendFile(__dirname + '/index.html');
 });
