d6cb9071f550a2bc60ab74cd9c784cc1e6f30f83
lewpoly <ldixon.design@gmail.com>
2022-06-09 20:51:51 -0400

    formatting

--- api/rap-names-express/server.js
+++ api/rap-names-express/server.js
@@ -1,71 +1,87 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 2121;
+require('dotenv').config();
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'rap';
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (_request, response) => {
+  db.collection('rappers')
+    .find()
+    .sort({ likes: -1 })
+    .toArray()
+    .then((data) => {
+      response.render('index.ejs', { info: data });
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-
-app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
 app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
+  db.collection('rappers')
+    .insertOne({
+      stageName: request.body.stageName,
+      birthName: request.body.birthName,
+      likes: 0,
     })
-    .catch(error => console.error(error))
-})
+    .then((_result) => {
+      console.log('Rapper Added');
+      response.redirect('/');
+    })
+    .catch((error) => console.error(error));
+});
 
 app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+  db.collection('rappers')
+    .updateOne(
+      {
+        stageName: request.body.stageNameS,
+        birthName: request.body.birthNameS,
+        likes: request.body.likesS,
+      },
+      {
         $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
+          likes: request.body.likesS + 1,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: true,
+      }
+    )
+    .then(() => {
+      console.log('Added One Like');
+      response.json('Like Added');
     })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
-    })
-    .catch(error => console.error(error))
-
-})
+    .catch((error) => console.error(error));
+});
 
 app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
+  db.collection('rappers')
+    .deleteOne({ stageName: request.body.stageNameS })
+    .then((_result) => {
+      console.log('Rapper Deleted');
+      response.json('Rapper Deleted');
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
